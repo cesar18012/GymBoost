194c2c4a19d59126491e3f3e7719bb8234986239
cesar18012 <cesardelcastillo18012@gmail.com>
2023-12-29 12:59:24 +0100

    creacion y funcionalidades del carrito

--- js/cartas.js
+++ js/cartas.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
             url: '../json/imgcarrusel.json',
             dataType: 'json',
             success: function (carrusel) {
+                carruselContainer.empty(); // Limpiar el contenedor antes de agregar nuevas imágenes
                 $.each(carrusel, function (index, img) {
                     var carruselHTML = `
                         <div class="carousel-item ${index === 0 ? 'active' : ''}">
@@ -16,7 +17,7 @@ $(document).ready(function () {
                 });
             },
             error: function (error) {
-                console.error('Error al cargar el archivo JSON:', error);
+                console.error('Error al cargar el archivo JSON para carrusel:', error);
             }
         });
     }
@@ -27,50 +28,71 @@ $(document).ready(function () {
     }
 
     function cargarCartas() {
-        // Limpiar el contenedor antes de volver a cargar las cartas
-        cartasContainer.empty();
+        if (cartasContainer.length > 0) {
+            cartasContainer.empty(); // Limpiar el contenedor antes de volver a cargar las cartas
+            var anchoVentana = $(window).width();
 
-        // Obtener el ancho actual de la ventana
-        var anchoVentana = $(window).width();
+            $.ajax({
+                url: '../json/cartas.json',
+                dataType: 'json',
+                success: function (cartas) {
+                    $.each(cartas, function (index, carta) {
+                        var rutaImagen = obtenerRutaImagen(anchoVentana, carta);
 
-        $.ajax({
-            url: '../json/cartas.json',
-            dataType: 'json',
-            success: function (cartas) {
-                $.each(cartas, function (index, carta) {
-                    // Obtener la ruta de la imagen según el ancho de la ventana
-                    var rutaImagen = obtenerRutaImagen(anchoVentana, carta);
-
-                    var cartaHtml = `
-                        <div class="col-lg-4 col-md-12">
-                            <div class="card text-center border border-primary shadow-0">
-                                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                                    <img src="${rutaImagen}" class="img-fluid" />
-                                    <a href="#!">
-                                        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
-                                    </a>
+                        var cartaHtml = `
+                            <div class="col-lg-4 col-md-12">
+                                <div class="card text-center border border-primary shadow-0">
+                                    <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                                        <img src="${rutaImagen}" class="img-fluid" />
+                                        <a href="#!">
+                                            <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+                                        </a>
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">${carta.cardTitle}</h5>
+                                        <p class="card-text">${carta.cardText}</p>
+                                        <p class="card-text">Precio: $${carta.precio}</p>
+                                        <button type="button" class="btn btn-primary agregar-al-carrito" data-id="${index}">
+                                            ${carta.buttonText}
+                                        </button>
+                                    </div>
+                                    <div class="card-footer">${carta.cardFooter}</div>
                                 </div>
-                                <div class="card-body">
-                                    <h5 class="card-title">${carta.cardTitle}</h5>
-                                    <p class="card-text">${carta.cardText}</p>
-                                    <button type="button" class="btn btn-primary">${carta.buttonText}</button>
-                                </div>
-                                <div class="card-footer">${carta.cardFooter}</div>
-                            </div>
-                        </div>`;
+                            </div>`;
 
-                    cartasContainer.append(cartaHtml);
-                });
-            },
-            error: function (error) {
-                console.error('Error al cargar el archivo JSON:', error);
-            }
-        });
+                        cartasContainer.append(cartaHtml);
+                    });
+
+                    // Manejar clic en el botón "Agregar al carrito"
+                    $('.agregar-al-carrito').on('click', function () {
+                        var productId = $(this).data('id');
+                        var selectedProduct = cartas[productId];
+                        agregarAlCarrito(selectedProduct);
+                    });
+                },
+                error: function (error) {
+                    console.error('Error al cargar el archivo JSON para cartas:', error);
+                }
+            });
+        }
     }
+
+    function agregarAlCarrito(producto) {
+        var carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+        carrito.push(producto);
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        actualizarCarrito();
+    }
+
+    function actualizarCarrito() {
+        cargarCarrito(); // Llama a la función cargarCarrito para actualizar la visualización del carrito
+    }
+    
 
     // Cargar las funciones
     cargarCartas();
     cargarCarrusel();
+    actualizarCarrito();
 
     // Escuchar cambios en el tamaño de la ventana
     $(window).resize(function () {
--- js/proteina.js
+++ js/proteina.js
@@ -1,12 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // Carrito de compras (inicialmente vacío)
+  var carrito = [];
+
   // Cargar datos desde el archivo JSON
-  $.getJSON('../json/proteina.json', function(data) {
+  $.getJSON('../json/proteina.json', function (data) {
     // Filtrar productos únicos por id
     var uniqueProducts = Array.from(new Set(data.map(producto => producto.id)))
       .map(id => data.find(producto => producto.id === id));
 
     // Llenar el menú desplegable con opciones dinámicamente
-    $.each(uniqueProducts, function(index, producto) {
+    $.each(uniqueProducts, function (index, producto) {
       $('#dropdownMenuLink').append(`<option value="${producto.id}">${producto.id}</option>`);
     });
 
@@ -14,7 +17,7 @@ $(document).ready(function() {
     renderizarProductos(data);
 
     // Manejar el cambio en el menú desplegable
-    $('#dropdownMenuLink').change(function() {
+    $('#dropdownMenuLink').change(function () {
       // Obtener el valor seleccionado
       var selectedProductId = $(this).val();
 
@@ -32,11 +35,23 @@ $(document).ready(function() {
       // Llamar a la función de renderizado para mostrar el producto seleccionado
       renderizarProductos(selectedProducts);
     });
+
+    // Manejar clic en el botón "Añadir al carrito"
+    $(document).on('click', '.btn-add-to-cart', function () {
+      var productId = $(this).data('id');
+      var selectedProduct = data.find(producto => producto.id === productId);
+
+      // Agregar el producto al carrito
+      agregarAlCarrito(selectedProduct);
+
+      // Actualizar la visualización del carrito (puedes llamar a actualizarCarrito si es necesario)
+      // actualizarCarrito();
+    });
   });
 
   // Función de renderizado que toma una lista de productos y los muestra en la página
   function renderizarProductos(productos) {
-    $.each(productos, function(index, product) {
+    $.each(productos, function (index, product) {
       var imageSrc = $(window).width() < 768 ? product.imagesmall : product.imagen;
 
       var cardHtml = `
@@ -44,9 +59,9 @@ $(document).ready(function() {
           <div class="card rounded shadow-sm border-3">
             <div class="card-body p-4">
               <img src="${imageSrc}" alt="" class="img-fluid d-block mx-auto mb-3">
-              <h5><a href="#" class="text-dark">${product.nombre}</a></h5>
+              <h5><a href="#" class="text-dark">${product.cardTitle}</a></h5>
               <p class="small text-muted font-italic">${product.descripcion}</p>
-              <button class="btn btn-primary">${product.boton}</button>
+              <button class="btn btn-primary btn-add-to-cart" data-id="${product.id}">Añadir al carrito</button>
             </div>
           </div>
         </div>
@@ -56,4 +71,14 @@ $(document).ready(function() {
       $('#productos-container').append(cardHtml);
     });
   }
+
+  // Función para agregar un producto al carrito
+  function agregarAlCarrito(producto) {
+    carrito.push(producto);
+
+    // Guardar el carrito en el almacenamiento local
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+  }
+
+  // Otras funciones y lógica aquí...
 });
